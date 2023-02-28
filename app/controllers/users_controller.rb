b25eec56902f947ff8098b1085c7087c2b59cd9c
andres lloret <andreslloretsz@gmail.com>
2023-02-28 14:21:12 +0100

    done

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,20 @@
+class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      # Redirect to a different page, such as the home page
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+end
